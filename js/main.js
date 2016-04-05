cb66e4367fa8791541e23b69d082d44ac087320a
justbeother <justbeother1@gmail.com>
2016-04-05 10:21:57 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,15 @@
 function showHost() {
-    alert(location.host);
+    alert("Хост: " + location.host;
+}
+
+function showBrowser() {
+    alert(navigator.userAgent);
+}
+
+function changeHeadSize() {
+    if (document.h1.size.valueOf() === new String("10").valueOf()) {
+        document.h1.size = "";
+    } else {
+        document.h1.size = "10";
+    }
 }
