4009cf86c1de39f54fee0422a47d7a2706a01660
justbeother <justbeother1@gmail.com>
2016-04-05 10:41:59 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,15 +1,17 @@
+function changeBckgColor() {
+    if (document.body.style.background.valueOf() === new String("black").valueOf()) {
+        document.body.style.background = "";
+    } else {
+        document.body.style.background = "black";
+        alert("Darkness falls across the land...
+The midnight hour is close at hand...");
+    }
+}
+
 function showHost() {
     alert("Хост: " + location.host);
 }
 
 function showBrowser() {
     alert(navigator.userAgent);
-}
-
-function changeHeadSize() {
-    if (document.h1.color.valueOf() === new String("red").valueOf()) {
-        document.body.h1.size = "";
-    } else {
-        document.h1.color = "red";
-    }
 }
